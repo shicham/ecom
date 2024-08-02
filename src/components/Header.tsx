6181b56579044627cc8d0e0ef2ed75dafe714b3b
hicham <sass.hicham@gmail.com>
2024-08-02 14:52:17 +0200

    init

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
 		<header>
 			<Link href="" className="p-2 mr-4 border-2 border-2 border-transparent rounded-full text-primary w-6 h-6 inline-flex relative">
 				
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
 			</Link>
 			<div className="flex mr-24 mt-2 ml-0">
 				<Link href=""><Image src='/logo.svg' width={100} height={24}/></Link>
