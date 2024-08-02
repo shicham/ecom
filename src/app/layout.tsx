fae0ec8f87a6e20af26cffa5ae704a02d0a3eb3c
hicham <sass.hicham@gmail.com>
2024-08-02 14:34:30 +0200

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Header from "@/components/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +15,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+	<html lang="en">
+		<body className={inter.className}>
+			<div className="manager">
+				<div className="page">
+					<div className="main-container">
+						<Header />
+						{children}
+					</div>
+				</div>
+			</div>
+		</body>
+	</html>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { Menu } from 'lucide-react';
+import Link from "next/link";
+import Image from 'next/image'
+export default function Header({ user, type = 'desktop', menuItems }: HeaderProps) {
+  return (
+		<header>
+			<Link href="" className="p-2 mr-4 border-2 border-2 border-transparent rounded-full text-primary w-6 h-6 inline-flex relative">
+				
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
+			</Link>
+			<div className="flex mr-24 mt-2 ml-0">
+				<Link href=""><Image src='/logo.svg' width={100} height={24}/></Link>
+			</div>
+			<div>
+			abcd
+			</div>
+			
+		</header>
+  )
+ };
