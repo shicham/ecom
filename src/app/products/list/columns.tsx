23344926b3bf0a1387194b066a55c61ca2fc55bb
hicham <sass.hicham@gmail.com>
2024-08-13 11:05:13 +0200

    listing

--- src/app/products/list/columns.tsx
+++ src/app/products/list/columns.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
-import { Images } from "@radix-ui/react-icons"
 import Link from 'next/link'
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
